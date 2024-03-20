d08551294a7171c403c6a74d0d2bb9ca2ee7b0d6
Sasha <132772266+notpatern@users.noreply.github.com>
2024-03-20 17:23:58 +0100

    feat: Created TextBatcher.cs

---
+++ srt-text-splitter/Services/TextBatcher.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Nodes;
+
+namespace srt_text_splitter.Services;
+
+public class TextBatcher
+{
+    public JsonObject Execute(int maxWordsPerBatch, JsonObject json)
+    {
+        // TODO: If first file, do max words per max words and populate something with the time codes used If not, check between those time codes and batch the text
+    }
+}
