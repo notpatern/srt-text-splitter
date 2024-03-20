5e387f68f830b7de9235a4d7463446e4a84d321d
Sasha <132772266+notpatern@users.noreply.github.com>
2024-03-20 14:45:54 +0100

    feat: Write a .json file from a JsonObject

--- srt-text-splitter/Services/JsonFileWriter.cs
+++ srt-text-splitter/Services/JsonFileWriter.cs
@@ -1,6 +1,17 @@
-﻿namespace srt_text_splitter.Services;
+﻿using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace srt_text_splitter.Services;
 
 public class JsonFileWriter
 {
-    
+    public void Execute(JsonObject json, string outputPath, string fileName)
+    {
+        string jsonToString = JsonSerializer.Serialize(json, new JsonSerializerOptions { IncludeFields = true, WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
+        
+        StreamWriter streamWriter = new StreamWriter(outputPath + Path.DirectorySeparatorChar + fileName + ".json", false, Encoding.UTF8);
+        streamWriter.Write(jsonToString);
+    }
 }
