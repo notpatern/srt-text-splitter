e0871a1a84171f569f70af3c21fa1ee2b0ebb0df
Sasha <132772266+notpatern@users.noreply.github.com>
2024-03-25 11:00:29 +0100

    feat: DataCollection.cs

---
+++ srt-text-splitter/Structures/DataCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Nodes;
+
+namespace srt_text_splitter.Structures;
+
+public struct DataCollection(List<double[]>? batchTimeCodes = null, JsonObject? json = null)
+{
+    public List<double[]>? BatchTimeCodes { get; set; } = batchTimeCodes;
+    public JsonObject? Json { get; set; } = json;
+}
