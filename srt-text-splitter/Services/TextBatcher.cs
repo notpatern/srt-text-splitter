e681a26ceb09554fbe3e020e81f48cd5a79a02cb
Sasha <132772266+notpatern@users.noreply.github.com>
2024-03-26 11:37:11 +0100

    feat: after first batch iteration logic added

--- srt-text-splitter/Services/TextBatcher.cs
+++ srt-text-splitter/Services/TextBatcher.cs
@@ -2,20 +2,62 @@
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using srt_text_splitter.Helper;
 using srt_text_splitter.Structures;
 
 namespace srt_text_splitter.Services;
 
 public class TextBatcher
 {
-    public DataCollection Execute(int maxWordsPerBatch, JsonObject? json, List<double[]>? batchTimeCodes)
+    public DataCollection Execute(int maxWordsPerBatch, JsonObject json, List<double[]>? batchTimeCodes)
     {
         return batchTimeCodes != null ? RunBatchWithTimeCodes(batchTimeCodes, json) : RunBatchWithMaxWords(maxWordsPerBatch, json);
     }
 
-    private DataCollection RunBatchWithTimeCodes(List<double[]>? batchTimeCodes, JsonObject? json)
+    private DataCollection RunBatchWithTimeCodes(List<double[]> batchTimeCodes, JsonObject json)
     {
-        return new DataCollection(batchTimeCodes, json);
+        PreviousTimeCodesFinder previousTimeCodesFinder = new PreviousTimeCodesFinder();
+        List<int[]> inOutIndexes = new List<int[]>();
+        JsonObject newJson = new JsonObject();
+
+        foreach (var inOutTimeCodes in batchTimeCodes)
+        {
+            int[] currentInOutIndexes = previousTimeCodesFinder.Execute(inOutTimeCodes, json);
+            if (currentInOutIndexes[1] == 0)
+            {
+                break;
+            }
+            inOutIndexes.Add(currentInOutIndexes);
+        }
+
+        for(int index = 1; index <= inOutIndexes.Count; index++)
+        {
+            string key = index.ToString();
+            string batchIn = json[inOutIndexes[index-1][0].ToString()]["In"].ToString();
+            string batchOut = json[inOutIndexes[index-1][1].ToString()]["Out"].ToString();
+            string batchContent = "";
+            for (int currentKeyIndex = inOutIndexes[index-1][0]; currentKeyIndex < inOutIndexes[index-1][1]; currentKeyIndex++)
+            {
+                string currentKey = currentKeyIndex.ToString();
+                batchContent += json[currentKey]["Content"];
+            }
+            Dictionary<string, string> valueDictionary =
+                new Dictionary<string, string>()
+                {
+                    ["In"] = batchIn,
+                    ["Out"] = batchOut,
+                    ["Content"] = batchContent
+                };
+                
+            string valueString = JsonSerializer.Serialize(valueDictionary,
+                new JsonSerializerOptions { IncludeFields = true, WriteIndented = true});
+                
+            JsonNode? value = JsonNode.Parse(valueString);
+                
+            newJson.Add(key, value);
+        }
+        
+        return new DataCollection(batchTimeCodes, newJson);
     }
 
     private DataCollection RunBatchWithMaxWords(int maxWordPerBatch, JsonObject? json)
@@ -35,7 +77,7 @@ public class TextBatcher
             string keyIndex = index.ToString();
             wordAmount += json[keyIndex]!["Content"]!.ToString().Split(delimiters,StringSplitOptions.RemoveEmptyEntries).Length;
             
-            if (wordAmount > maxWordPerBatch)
+            if (wordAmount > maxWordPerBatch || index == json.Count)
             {
                 string previousKey = (index - 1).ToString();
                 string nextKey = (index + 1).ToString();
@@ -54,7 +96,7 @@ public class TextBatcher
                     };
                 
                 string valueString = JsonSerializer.Serialize(valueDictionary,
-                    new JsonSerializerOptions { IncludeFields = true, WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping});
+                    new JsonSerializerOptions { IncludeFields = true, WriteIndented = true });
                 
                 JsonNode? value = JsonNode.Parse(valueString);
                 
